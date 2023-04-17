017bdb6c25de00880ba0fbd23c4cd74cc456d337
Hiroto-Shida <uecshida@gmail.com>
2023-04-18 01:08:25 +0900

    signup & signin & signout with firebase

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import { Heading } from '@chakra-ui/react'
+import { AuthGuard } from '@src/feature/auth/component/AuthGuard/AuthGuard'
+
+
+const Page: NextPage = () => {
+  return (
+    <AuthGuard>
+      <Heading>Chakra UI</Heading>
+    </AuthGuard>
+  )
+}
+
+export default Page

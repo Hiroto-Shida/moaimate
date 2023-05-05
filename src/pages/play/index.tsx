43daea806421aa98491c9a124b092b9645d0a83a
Hiroto-Shida <uecshida@gmail.com>
2023-05-05 20:35:18 +0900

    KeyEventコンポーネントの移動

--- src/pages/play/index.tsx
+++ src/pages/play/index.tsx
@@ -14,7 +14,7 @@ import * as THREE from 'three'
 import { Canvas, Props, useFrame, useLoader } from '@react-three/fiber';
 import { Environment, OrbitControls } from '@react-three/drei';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import useKeyboard from './useKeyboard';
+import useKeyboard from '@src/component/KeyEvent/useKeyboard';
 import { CameraControls } from '@react-three/drei';
 
 const Page: NextPage = () => {
